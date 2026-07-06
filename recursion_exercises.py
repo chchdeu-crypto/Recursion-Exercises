a41ad3c497b708b275efc7653b6a3c1d82f50fe9
chaim <chchdeu@gmail.com>
2026-07-06 15:49:14 +0300

    mission_2

--- recursion_exercises.py
+++ recursion_exercises.py
@@ -2,6 +2,13 @@
 def power(base,exponent):
     if exponent<=0:
         return 1
-    return base*(power(base,exponent-1))
+    return base*power(base,exponent-1)
     
 print(power(2,4))
+
+#mission 2
+def factorial(n):
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+print(factorial(5))
